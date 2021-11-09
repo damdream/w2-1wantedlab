67c8543ff18891171f0d5e0544795db582898a14
damdream <possible_11@naver.com>
2021-11-09 23:22:29 +0900

    fix: url 수정

--- crud/urls.py
+++ crud/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 import crud.views as views
+from .views import companyEnrollmentView
 
 urlpatterns = [
         path('data/injection/', views.DataInjectionView.as_view(),name='data_injection'),
+
+        path('company/enrollment', companyEnrollmentView.as_view())
 ]
